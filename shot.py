a90681b52f2342a1bfcb970d494a723748db2ca4
Jesus19-crypto <jfierro@csu.fullerton.edu>
2026-07-07 23:20:10 -0700

    Task:Finished prototype game

---
+++ shot.py
@@ -0,0 +1,15 @@
+import pygame
+from circleshape import CircleShape
+from constants import SHOT_RADIUS
+
+class Shot(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.velocity = pygame.Vector2(0, 0)
+    
+    def draw(self, screen):
+        return pygame.draw.circle(screen, "white", self.position, self.radius)
+    
+    
+    def update(self, dt):
+        self.position += self.velocity * dt
